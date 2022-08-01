6a625388914c488b713f7987139f5576cf73851f
USER <119haa@Naver.com>
2022-08-01 14:26:50 +0900

    Initial commit

---
+++ src/main/java/com/example/communityPz/CommunityPzApplication.java
@@ -0,0 +1,13 @@
+package com.example.communityPz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityPzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityPzApplication.class, args);
+	}
+
+}
